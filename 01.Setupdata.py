41224823dd93bcdebd2d617b8c14ea651817a2ce
Jasonw <ningweiw@gmail.com>
2017-04-08 02:00:56 -0700

    linear model result

--- 01.Setupdata.py
+++ 01.Setupdata.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -9,26 +10,27 @@ sns.set_style('white')
 N = 100 # number of points per class
 D = 2 # dimensionality
 K = 3 # number of classes
-X = np.zeros((N*K,D)) # data matrix
+data_mat = np.zeros((N * K, D)) # data matrix
 Class = np.zeros(N * K, dtype='uint8') # class labels
 for j in range(K):
   i = range(N*j,N*(j+1))
   r = np.linspace(0.01,1,N) # radius
   t = np.linspace(j*4,(j+1)*4,N) + np.random.randn(N)*0.25 # theta
-  X[i] = np.c_[r*np.sin(t), r*np.cos(t)]
+  data_mat[i] = np.c_[r * np.sin(t), r * np.cos(t)]
   Class[i] = j
 
 # visualize the data set
 # plt.style.use('ggplot')
 fig, ax = plt.subplots()
-ax.scatter(X[Class==0, 0], X[Class==0, 1], label='class 0', color='b')
-ax.scatter(X[Class==1, 0], X[Class==1, 1], label='class 1', color='g')
-ax.scatter(X[Class==2, 0], X[Class==2, 1], label='class 2', color='r')
+ax.scatter(data_mat[Class == 0, 0], data_mat[Class == 0, 1], label='class 0', color='b')
+ax.scatter(data_mat[Class == 1, 0], data_mat[Class == 1, 1], label='class 1', color='g')
+ax.scatter(data_mat[Class == 2, 0], data_mat[Class == 2, 1], label='class 2', color='r')
 sns.despine()
 ax.legend()
-ax.set(xlabel='X1', ylabel='X2', title='ternary classification data set')
-fig.show()
+ax.set(xlabel='X', ylabel='Y', title='ternary classification data set')
+# fig.show()
+fig.savefig('pic/data_set.png', dpi=fig.dpi)
 
 # save the data set to csv file
-data_output = np.column_stack((X, Class))
-np.savetxt('classification_data.csv', data_output, header='X1,X2,Class', comments='', delimiter=',')
+data_output = np.column_stack((data_mat, Class))
+np.savetxt('classification_data.csv', data_output, header='X,Y,Class', comments='', delimiter=',')
---
+++ 02.LinearModel.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import sklearn
+from sklearn.linear_model import LogisticRegression
+import matplotlib
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set_style('white')
+
+my_data = np.genfromtxt('classification_data.csv', delimiter=',', skip_header=True)
+# print(my_data.shape)
+
+# train a glm in multinomial family
+model_linear = LogisticRegression(solver='lbfgs', multi_class='multinomial').fit(my_data[:,:2], my_data[:,2])
+
+# print out the summary of the linear model
+print(model_linear.intercept_)
+print(model_linear.coef_)
+
+class_pred = model_linear.predict(my_data[:,:2])
+class_true = my_data[:,2]
+print(sklearn.metrics.classification_report(class_true, class_pred))
+'''
+             precision    recall  f1-score   support
+        0.0       0.50      0.54      0.52       100
+        1.0       0.57      0.56      0.56       100
+        2.0       0.48      0.45      0.47       100
+avg / total       0.52      0.52      0.52       300
+
+note that 
+1. precision is the positive predictive value
+2. recall is the sensitivity or the true positive rate
+3. f1-score is a kind of weighted average of precision and recall. it reaches its best value at 1 and worst at 0.
+   https://en.wikipedia.org/wiki/F1_score
+'''
+
+# plot the classification result
+# plot the decision boundary. for that, we will assign a color to each point in the mesh [x_min, x_max]*[y_min, y_max].
+x_min, x_max = my_data[:, 0].min() - .5, my_data[:, 0].max() + .5
+y_min, y_max = my_data[:, 1].min() - .5, my_data[:, 1].max() + .5
+h = 0.01  # step size in the mesh
+xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+Class = model_linear.predict(np.c_[xx.ravel(), yy.ravel()])
+Class = Class.reshape(xx.shape)
+
+# plot the classification region
+fig, ax = plt.subplots()
+color_style = matplotlib.colors.ListedColormap(['#99d9ea', '#b5e61d','#ffaec9'])
+ax.pcolormesh(xx, yy, Class, cmap=color_style)
+
+# plot the training data set
+ax.scatter(my_data[class_true == 0, 0], my_data[class_true == 0, 1], label='class 0', color='b', edgecolor='w', linewidth='1')
+ax.scatter(my_data[class_true == 1, 0], my_data[class_true == 1, 1], label='class 1', color='g', edgecolor='w', linewidth='1')
+ax.scatter(my_data[class_true == 2, 0], my_data[class_true == 2, 1], label='class 2', color='r', edgecolor='w', linewidth='1')
+sns.despine()
+ax.legend()
+ax.set(xlabel='X', ylabel='Y', title='classification result for linear model')
+# fig.show()
+fig.savefig('pic/linear_result.png', dpi=fig.dpi)
