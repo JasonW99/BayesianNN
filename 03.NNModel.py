c80c7b9b321fe56c9a9ea4805fe4ef93eae5f0ae
Jasonw <ningweiw@gmail.com>
2017-04-08 23:26:28 -0700

    neural network model result

--- 03.NNModel.py
+++ 03.NNModel.py
@@ -2,38 +2,33 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
-import sklearn
-from sklearn.linear_model import LogisticRegression
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.utils import np_utils
 import matplotlib
 import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set_style('white')
 
+# load the data set and split it into response and predictor
 my_data = np.genfromtxt('classification_data.csv', delimiter=',', skip_header=True)
-# print(my_data.shape)
+X = my_data[:,:2]
+class_true = my_data[:, 2]
+Class_onehot = np_utils.to_categorical(class_true) # one hot encoding
 
-# train a glm in multinomial family
-model_linear = LogisticRegression(solver='lbfgs', multi_class='multinomial').fit(my_data[:,:2], my_data[:,2])
+# create the model
+model_NN = Sequential()
+model_NN.add(Dense(100, input_dim=2, kernel_initializer='normal', activation='relu'))
+model_NN.add(Dense(3, activation='softmax'))
+model_NN.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+model_NN.fit(X, Class_onehot, epochs=1200, batch_size=100)
 
-# print out the summary of the linear model
-print(model_linear.intercept_)
-print(model_linear.coef_)
-
-class_pred = model_linear.predict(my_data[:,:2])
-class_true = my_data[:,2]
-print(sklearn.metrics.classification_report(class_true, class_pred))
 '''
-             precision    recall  f1-score   support
-        0.0       0.50      0.54      0.52       100
-        1.0       0.57      0.56      0.56       100
-        2.0       0.48      0.45      0.47       100
-avg / total       0.52      0.52      0.52       300
-
-note that 
-1. precision is the positive predictive value
-2. recall is the sensitivity or the true positive rate
-3. f1-score is a kind of weighted average of precision and recall. it reaches its best value at 1 and worst at 0.
-   https://en.wikipedia.org/wiki/F1_score
+# evaluate the classification result
+scores = model_NN.evaluate(X, Class_onehot)
+print("\n%s: %.2f%%" % (model_NN.metrics_names[1], scores[1]*100))
+class_pred = model_NN.predict(X)
+class_pred = np.argmax(class_pred, axis=1)
 '''
 
 # plot the classification result
@@ -42,7 +37,8 @@ x_min, x_max = my_data[:, 0].min() - .5, my_data[:, 0].max() + .5
 y_min, y_max = my_data[:, 1].min() - .5, my_data[:, 1].max() + .5
 h = 0.01  # step size in the mesh
 xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
-Class_region = model_linear.predict(np.c_[xx.ravel(), yy.ravel()])
+Class_region = model_NN.predict(np.c_[xx.ravel(), yy.ravel()])
+Class_region = np.argmax(Class_region, axis=1)
 Class_region = Class_region.reshape(xx.shape)
 
 # plot the classification region
@@ -56,6 +52,6 @@ ax.scatter(my_data[class_true == 1, 0], my_data[class_true == 1, 1], label='clas
 ax.scatter(my_data[class_true == 2, 0], my_data[class_true == 2, 1], label='class 2', color='r', edgecolor='w', linewidth='1')
 sns.despine()
 ax.legend()
-ax.set(xlabel='X', ylabel='Y', title='classification result for linear model')
-# fig.show()
-fig.savefig('pic/linear_result.png', dpi=fig.dpi)
+ax.set(xlabel='X', ylabel='Y', title='classification result for neural network model')
+fig.show()
+fig.savefig('pic/NN_result.png', dpi=fig.dpi)
